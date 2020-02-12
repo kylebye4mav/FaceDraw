d66ba2f789fec0bc69ac90522422f7df7085a8b9
kylebye4mav <54649445+kylebye4mav@users.noreply.github.com>
2020-02-11 20:47:02 -0600

    Update Oval.java

--- shapes/Oval.java
+++ shapes/Oval.java
@@ -1,9 +1,50 @@
-package shapes;
+package shapes.basic;
 
+// javax.swing packages
+import javax.swing.JPanel;
+
+// java.awt packages
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * @author Kyle Bye
+ */
 public class Oval extends Shape {
 
+	private int radiusX, radiusY;
+
+	// Getters
+	public int getRadiusX() { return radiusX; }
+	public int getRadiusY() { return radiusY; }
+
+	// Setters
+	public void setRadiusX(int radiusXIn) { radiusX = radiusXIn; }
+	public void setRadiusY(int radiusYIn) { radiusY = radiusYIn; }
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		g.setColor(color);
+		g.fillOval(x, y, getWidth(), getHeight());
+	}
+
+	// Constructors
 	public Oval() {
-		super();
+		this(0, 0, 0, 0, Color.WHITE);
+	}
+
+	public Oval(int radiusXIn, int radiusYIn) {
+		this(0, 0, radiusXIn, radiusYIn, Color.WHITE);
+	}
+
+	public Oval(int x, int y, int radiusXIn int radiusYIn) {
+		this(x, y, radiusXIn, radiusYIn, Color.WHITE);
+	}
+
+	public Oval(int x, int y, int radiusXIn, int radiusYIn, Color colorIn) {
+		super(x, y, radiusXIn*2, radiusYIn*2, colorIn);
 	}
 
 }
